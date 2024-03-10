b7b98af56de043f1165260d69ddaa31659841010
= <mmohmo@yahoo.com>
2024-03-09 22:45:09 -0500

    project pictures added

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -79,8 +79,8 @@ export const Navbar = () => {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link to="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">BOUTIQUE</span>
-             BOUTIQUE
+              <span className="sr-only">Big Dream Farm</span>
+             Big Dream Farm
             </Link>
             <button
               type="button"
