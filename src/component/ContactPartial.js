4ce175c86dc5f24f8c65b5bc19b42828bdc55b54
= <mmohmo@yahoo.com>
2024-03-18 13:40:54 -0400

    contact partial padding updated

--- src/component/ContactPartial.js
+++ src/component/ContactPartial.js
@@ -4,26 +4,26 @@ import { SmoothScrolling } from './SmoothScrolling'
 
 export const ContactPartial = () => {
   return (
-    <div className="relative isolate overflow-hidden py-24 md:py-64 sm:py-32">
+    <div className="relative isolate overflow-hidden py-14 md:py-24 sm:py-12">
       <div>
         <img 
           src="/images/leaf.jpg" 
           alt="leaf"
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
         />
-        <div className="flex justify-end mr-2 md:mr-24">
+        <div className="flex justify-end ml-3 md:ml-0 mr-3 md:mr-24 ">
           <div className="bg-white rounded-3xl w-96 h-1/2 px-12 py-16">
             <div>
-              <div>
-                <h3>Location</h3>
+              <div className="py-3">
+                <h3 className="text-xl leading-10">Location</h3>
               </div>
               <div>
-                <h2>Soumppou Kaffo Farm</h2>
-                <h4>Big Dream Farm</h4>
+                <h2 className="text-2xl pt-4 text-green-800 tracking-tight font-semibold">Big Dream Farm</h2>
+                
                 <p>Big Dream Farm products can be found in main cities of following places.</p>
                 <div className="flex">
                   <div>
-                    <div className="flex">
+                    <div className="md:flex block">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#008000" className="w-16 h-16">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
@@ -36,7 +36,7 @@ export const ContactPartial = () => {
                         </p>
                       </div>
                     </div>
-                    <div className="flex">
+                    <div className="md:flex block">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#008000" className="w-16 h-16">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
@@ -49,7 +49,7 @@ export const ContactPartial = () => {
                         </p>
                       </div>
                     </div>
-                    <div className="flex">
+                    <div className="md:flex block">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#008000" className="w-16 h-16">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
