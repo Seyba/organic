2527f694aced8d204696aef740e1a08e3339ccd5
= <mmohmo@yahoo.com>
2024-03-18 14:21:38 -0400

    contact partial component fully styled

--- src/component/ContactPartial.js
+++ src/component/ContactPartial.js
@@ -14,56 +14,75 @@ export const ContactPartial = () => {
         <div className="flex justify-end ml-3 md:ml-0 mr-3 md:mr-24 ">
           <div className="bg-white rounded-3xl w-96 h-1/2 px-12 py-16">
             <div>
-              <div className="py-3">
+              <div className="">
                 <h3 className="text-xl leading-10">Location</h3>
               </div>
               <div>
-                <h2 className="text-2xl pt-4 text-green-800 tracking-tight font-semibold">Big Dream Farm</h2>
+                <h2 className="text-2xl py-4 text-green-800 tracking-tight font-semibold">Big Dream Farm</h2>
                 
-                <p>Big Dream Farm products can be found in main cities of following places.</p>
+                <p className="text-lg py-2 font-thin leading-7 tracking-wide">Big Dream Farm products can be found in main cities of following places.</p>
                 <div className="flex">
                   <div>
-                    <div className="md:flex block">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#008000" className="w-16 h-16">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                      </svg>
-                      <div>
-                        <h4>New York, USA:</h4>
-                        <p>
+                    <div className="md:flex md:items-center block">
+                      <div className="mr-0 md:mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"   viewBox="0 0 24 24" strokeWidth={1.5} stroke="#008000"  className="w-16 h-16">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                        </svg>
+                      </div>
+                      <div className="py-2">
+                        <h4 className="text-lg font-bold text-green-800">New York, USA:</h4>
+                        <p className="text-lg py-2 font-thin leading-7 tracking-wide">
                           299 Park Avenue New York,
                           New York 10171, USA.
                         </p>
                       </div>
                     </div>
-                    <div className="md:flex block">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#008000" className="w-16 h-16">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                      </svg>
-                      <div>
-                        <h4>Philadelphia, USA:</h4>
-                        <p>
+                    <div className="md:flex md:items-center block">
+                      <div className="mr-0 md:mr-4">
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          fill="none" 
+                          viewBox="0 0 24 24" 
+                          strokeWidth={1.5} 
+                          stroke="#008000" 
+                          className="w-16 h-16"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                        </svg>
+                      </div>
+                      <div className="py-2">
+                        <h4 className="text-lg font-bold text-green-800">Philadelphia, USA:</h4>
+                        <p className="text-lg py-2 font-thin leading-7 tracking-wide">
                           299 Park Avenue New York,
                           New York 10171, USA.
                         </p>
                       </div>
                     </div>
-                    <div className="md:flex block">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#008000" className="w-16 h-16">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                      </svg>
-                      <div>
-                        <h4>London, UK:</h4>
-                        <p>
-                          30 Stamford Street, Base Way,
-                          London 5478, UK.
+                    <div className="md:flex md:items-center block">
+                      <div className="mr-0 md:mr-4">
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          fill="none" 
+                          viewBox="0 0 24 24" 
+                          strokeWidth={1.5} 
+                          stroke="#008000" 
+                          className="w-16 h-16"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                        </svg>
+                      </div>
+                      <div className="py-2">
+                        <h4 className="text-lg font-bold text-green-800">London, UK:</h4>
+                        <p className="text-lg py-2 font-thin leading-7 tracking-wide">
+                          49 Washburn Farm Rd, Saugerties, NY 12477
+                          United States.
                         </p>
                       </div>
                     </div>
                   </div>
-                  <div></div>
                 </div>
               </div>
             </div>
