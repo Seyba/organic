63607d7897c199108862e1cd020ea74d5ad671d6
= <mmohmo@yahoo.com>
2024-03-17 23:00:22 -0400

    carousel heading updated for small devices

--- src/component/SoumppouCarousel.js
+++ src/component/SoumppouCarousel.js
@@ -48,12 +48,12 @@ export const SoumppouCarousel = () => {
                             <p className="text-xl tracking-tight font-semibold">100% organic foods in the market</p>
                         </div>
                         <div className="py-6">
-                            <h3 className="text-6xl font-bold">Halal Meat Source and Vegetable</h3>
-                            <p className="text-xl leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
+                            <h3 className="text-2xl md:text-6xl font-bold">Halal Meat Source and Vegetable</h3>
+                            <p className="md:text-xl text-lg leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
                         </div>
                         <div className="flex justify-center mt-3">
-                            <button className="border border-1 rounded-3xl py-3 px-10 mr-2">Shop Now</button>
-                            <button className="border border-1 rounded-3xl py-3 px-10 ml-2">Contact Us</button>
+                            <button className="border border-1 rounded-3xl py-3 px-10 mr-2 hover:bg-slate-100 hover:text-slate-900">Shop Now</button>
+                            <button className="border border-1 rounded-3xl py-3 px-10 ml-2 hover:bg-slate-100 hover:text-slate-900">Contact Us</button>
                         </div>
                     </div>
 
@@ -81,12 +81,12 @@ export const SoumppouCarousel = () => {
                             <p className="text-xl tracking-tight font-semibold">100% organic foods in the market</p>
                         </div>
                         <div className="py-6">
-                            <h3 className="text-6xl font-bold">Halal Meat Source and Vegetable</h3>
-                            <p className="text-xl leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
+                            <h3 className="text-2xl md:text-6xl font-bold">Halal Meat Source and Vegetable</h3>
+                            <p className="md:text-xl text-lg leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
                         </div>
                         <div className="flex justify-center mt-3">
-                            <button className="border border-1 rounded-3xl py-3 px-10 mr-2">Shop Now</button>
-                            <button className="border border-1 rounded-3xl py-3 px-10 ml-2">Contact Us</button>
+                            <button className="border border-1 rounded-3xl py-3 px-10 mr-2 hover:bg-slate-100 hover:text-slate-900">Shop Now</button>
+                            <button className="border border-1 rounded-3xl py-3 px-10 ml-2 hover:bg-slate-100 hover:text-slate-900">Contact Us</button>
                         </div>
                     </div>
                     {/* <div className="">
@@ -114,12 +114,12 @@ export const SoumppouCarousel = () => {
                             <p className="text-xl tracking-tight font-semibold">100% organic foods in the market</p>
                         </div>
                         <div className="py-6">
-                            <h3 className="text-6xl font-bold">Halal Meat Source and Vegetable</h3>
-                            <p className="text-xl leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
+                            <h3 className="text-2xl md:text-6xl font-bold">Halal Meat Source and Vegetable</h3>
+                            <p className="md:text-xl text-lg leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
                         </div>
                         <div className="flex justify-center mt-3">
-                            <button className="border border-1 rounded-3xl py-3 px-10 mr-2">Shop Now</button>
-                            <button className="border border-1 rounded-3xl py-3 px-10 ml-2">Contact Us</button>
+                            <button className="border border-1 rounded-3xl py-3 px-10 mr-2 hover:bg-slate-100 hover:text-slate-900">Shop Now</button>
+                            <button className="border border-1 rounded-3xl py-3 px-10 ml-2 hover:bg-slate-100 hover:text-slate-900">Contact Us</button>
                         </div>
                     </div>
                     {/* <div>
