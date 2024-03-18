8891813d4c913febfc8ef2dbcccda6175d077a56
= <mmohmo@yahoo.com>
2024-03-18 18:56:03 -0400

    home partial component created

---
+++ src/component/HomePartial.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const HomePartial = () => {
+  return (
+    <div>
+        <div className="grid md:grid-cols-2 gap-4">
+            <div>
+                <h2>image</h2>
+            </div>
+            <div>
+                <p>
+                    We are proud to be producing and raising much of the food for sale in the shop from the farm. 
+                    It is very labour intensive and not good for 'economies of scale' to have many small enterprises but we are working our way towards
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
