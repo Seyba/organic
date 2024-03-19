a67c5da6afc0524137e3a2a275b74c968cf5cf82
= <mmohmo@yahoo.com>
2024-03-18 23:12:28 -0400

    contact partial updated

--- src/component/ContactPartial.js
+++ src/component/ContactPartial.js
@@ -15,7 +15,7 @@ export const ContactPartial = () => {
           <div className="bg-white rounded-3xl w-96 h-1/2 px-12 py-16">
             <div>
               <div className="">
-                <h3 className="text-xl leading-10">Location</h3>
+                <h3 className="text-xl font-semibold text-green-950 leading-10">Location</h3>
               </div>
               <div>
                 <h2 className="text-2xl py-4 text-green-800 tracking-tight font-semibold">Big Dream Farm</h2>
@@ -38,6 +38,7 @@ export const ContactPartial = () => {
                         </p>
                       </div>
                     </div>
+                    
                     <div className="md:flex md:items-center block">
                       <div className="mr-0 md:mr-4">
                         <svg 
@@ -53,29 +54,7 @@ export const ContactPartial = () => {
                         </svg>
                       </div>
                       <div className="py-2">
-                        <h4 className="text-lg font-bold text-green-800">Philadelphia, USA:</h4>
-                        <p className="text-lg py-2 font-thin leading-7 tracking-wide">
-                          299 Park Avenue New York,
-                          New York 10171, USA.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="md:flex md:items-center block">
-                      <div className="mr-0 md:mr-4">
-                        <svg 
-                          xmlns="http://www.w3.org/2000/svg" 
-                          fill="none" 
-                          viewBox="0 0 24 24" 
-                          strokeWidth={1.5} 
-                          stroke="#008000" 
-                          className="w-16 h-16"
-                        >
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                        </svg>
-                      </div>
-                      <div className="py-2">
-                        <h4 className="text-lg font-bold text-green-800">London, UK:</h4>
+                        <h4 className="text-lg font-bold text-green-800">New York, USA:</h4>
                         <p className="text-lg py-2 font-thin leading-7 tracking-wide">
                           49 Washburn Farm Rd, Saugerties, NY 12477
                           United States.
