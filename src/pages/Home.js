f6b08cd2bd65923722a35cbd616298d0cda3bac2
= <mmohmo@yahoo.com>
2024-03-26 22:18:21 -0400

    green section title styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -70,9 +70,9 @@ export const Home = () => {
       <HomePartial/>
       <section>
         <div className="grid md:grid-cols-2 gap-0">
-          <div className="green-three py-8 grid place-items-center md:grid-cols-2 gap-2">
+          <div className="green-two py-8 grid place-items-center md:grid-cols-2 gap-2">
             <div className="">
-              <div className="flex justify-center items-center rounded-full green-four h-16 w-16">
+              <div className="flex justify-center items-center rounded-full green-four h-20 w-20">
                 {/* <img src="/images/crop.png" alt="crop" className="h-8 w-8 items-center"/> */}
                 <svg 
                   fill="#ffffff" 
@@ -96,22 +96,22 @@ export const Home = () => {
                 </svg>
               </div>
               <div>
-                <h3>23+</h3>
-                <h4>Products</h4>
+                <h3 className="text-white text-5xl font-semibold">23+</h3>
+                <h4 className="text-white text-3xl font-light">Products</h4>
               </div>
-              <div>
+              <div className="flex justify-center items-center rounded-full green-four h-20 w-20">
                 <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4.63495 14.4151L5.67396 12.2121C5.80734 11.9293 6.19266 11.9293 6.32604 12.2121L7.36505 14.4151L9.68859 14.7706C9.98671 14.8162 10.1055 15.1997 9.8897 15.4198L8.2087 17.1334L8.60542 19.5543C8.65637 19.8652 8.34456 20.1022 8.07781 19.9554L6 18.8118L3.92219 19.9554C3.65544 20.1022 3.34363 19.8652 3.39458 19.5543L3.7913 17.1334L2.1103 15.4198C1.89447 15.1997 2.01329 14.8162 2.31141 14.7706L4.63495 14.4151Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16.6349 14.4151L17.674 12.2121C17.8073 11.9293 18.1927 11.9293 18.326 12.2121L19.3651 14.4151L21.6886 14.7706C21.9867 14.8162 22.1055 15.1997 21.8897 15.4198L20.2087 17.1334L20.6054 19.5543C20.6564 19.8652 20.3446 20.1022 20.0778 19.9554L18 18.8118L15.9222 19.9554C15.6554 20.1022 15.3436 19.8652 15.3946 19.5543L15.7913 17.1334L14.1103 15.4198C13.8945 15.1997 14.0133 14.8162 14.3114 14.7706L16.6349 14.4151Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M10.6349 5.41515L11.674 3.21211C11.8073 2.9293 12.1927 2.9293 12.326 3.21211L13.3651 5.41515L15.6886 5.7706C15.9867 5.8162 16.1055 6.19974 15.8897 6.41976L14.2087 8.13337L14.6054 10.5543C14.6564 10.8652 14.3446 11.1022 14.0778 10.9554L12 9.81178L9.92219 10.9554C9.65544 11.1022 9.34363 10.8652 9.39458 10.5543L9.7913 8.13337L8.1103 6.41976C7.89447 6.19974 8.01329 5.8162 8.31141 5.7706L10.6349 5.41515Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g>
               </svg>
                 {/* <img src="/images/stars.png" alt="stars" className="h-16 w-16"/> */}
               </div>
               <div>
-                <h3>10+</h3>
+                <h3 className="text-white text-5xl font-semibold">10+</h3>
                 <h4>Years of Experience</h4>
               </div>
             </div>
             
             <div>
-              <div>
+              <div className="flex justify-center items-center rounded-full green-four h-20 w-20">
                 <svg fill="#ffffff" version="1.1" id="Layer_1"  xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xmlSpace="preserve" width="40px" height="40px">
                   <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                   <g id="SVGRepo_tracerCarrier" stroke-linecap="round" strokeLinejoin="round"></g>
@@ -124,10 +124,10 @@ export const Home = () => {
                 {/* <img src="/images/satisfaction.png" alt="satisfaction" className="h-16 w-16"/> */}
               </div>
               <div>
-                <h3>1500+</h3>
+                <h3 className="text-white text-5xl font-semibold">1500+</h3>
                 <h4>Satisfied Clients</h4>
               </div>
-              <div>
+              <div className="flex justify-center items-center rounded-full green-four h-20 w-20">
                 <svg fill="#ffffff" height="40px" width="40px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 372.483 372.483" xmlSpace="preserve">
                   <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                   <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -144,7 +144,7 @@ export const Home = () => {
                 {/* <img src="/images/team.png" alt="team" className="h-16 w-16"/> */}
               </div>
               <div>
-                <h3>55+</h3>
+                <h3 className="text-white text-5xl font-semibold">55+</h3>
                 <h4>Local Members</h4>
               </div>
             </div>
