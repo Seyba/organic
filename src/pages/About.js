75891271b482387c3a40b9c64d6efc72847951ba
= <mmohmo@yahoo.com>
2024-04-02 20:13:22 -0400

    about page banner updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,21 +8,18 @@ import SeyModal from '../component/SeyModal'
 export const About = () => {
   return (
     <>
-      <div className="banner-wrapper">
-        <div className="banner">
+      <div className="bg-green-950">
+        <div className="">
           <div className="py-2">
-            <h2 className="text-5xl font-semibold text-center py-2 animate-fade-down animate-once animate-duration-800 animate-ease-in-out">About Us</h2>
+            <h2 className="text-5xl text-white font-semibold text-center py-2 animate-fade-down animate-once animate-duration-800 animate-ease-in-out">About Us</h2>
           </div>
-          <Meta title={"About"}/>
-          <BreadCrumb title="About"/>
-          <div className="flex justify-center py-2">
-            <div>
-              <svg width="50px" height="100px" viewBox="0 0 247 300" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={{fillRule:"evenodd" ,clipRule:"evenodd", strokeLinecap:"round" ,strokeLinejoin:'round', strokeMiterlimit:'1.5'}}>
-                <path  className="wheel" d="M123.359,79.775l0,72.843" style={{fill:"none" ,stroke:"#fff" ,strokeWidth:"15px"}} />
-                <path  className="scroll" d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z" style={{fill:"none" ,stroke:"#fff" ,strokeWidth:"15px"}} />
-              </svg>
-            </div>
+          <div className="flex justify-end">
+            <Meta title={"About"}/>
           </div>
+          <div className="flex justify-end">
+            <BreadCrumb title="About"/>
+          </div>
+          
         </div>    
       </div>
       <div>
