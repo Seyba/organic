4c75b031eb414a2143927705de5fbb40da1c40e9
= <mmohmo@yahoo.com>
2024-04-20 17:07:09 -0400

    form links styled responsively

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,7 @@ export const Login = () => {
         <section className="relative isolate overflow-hidden bg-zinc-900 px-4 md:px-0 py-24 sm:py-32">
             <img
                 src="/images/brussel-sprouts.jpg"
-                alt=""
+                alt="brussel sprouts"
                 className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
             />
             <div className="flex">
@@ -44,12 +44,14 @@ export const Login = () => {
                     <div className='my-4'>
                         <button className="green-one hover:bg-green-900 text-white w-full py-2 text-lg font-semibold">Log In </button>
                     </div>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 my-4">
-                        <div>
-                            <Link className="tracking-tight border-r border-gray-900 md:pr-8 pr-2 py-2">Forgot My Username</Link>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 my-4 divide-x">
+                        <div className=" flex justify-center my-2">
+                            <Link className="font-semibold sm:font-medium ">Forgot My Username</Link>
+                            {/* <Link className="tracking-tight border-0 sm:items-left items-center sm:border-r border-gray-900 md:pr-8 pr-2 py-2">Forgot My Username</Link> */}
                         </div>
-                        <div>
-                            <Link className="md:pl-8 pl-2 tracking-tight">Forgot My Password</Link>
+                        <div className="flex justify-center my-2">
+                            {/* <Link className="md:pl-8 pl-2 tracking-tight">Forgot My Password</Link> */}
+                            <Link className="font-semibold sm:font-medium">Forgot My Password</Link> 
                         </div>
                     </div>
                     <div className="my-4 text-center">
