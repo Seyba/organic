35d91b65936865f1211f249a298d24edfae8cb24
= <mmohmo@yahoo.com>
2024-04-20 17:16:37 -0400

    login page fully styled

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,7 +15,7 @@ export const Login = () => {
                     <div className="flex mb-4">
                         <h2 className="md:text-3xl text-2xl mb-4 font-light leading-10">Log In </h2>
                         <div className="ml-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#227456" className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#133f2f" className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
                                 <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
                             </svg>
@@ -44,18 +44,16 @@ export const Login = () => {
                     <div className='my-4'>
                         <button className="green-one hover:bg-green-900 text-white w-full py-2 text-lg font-semibold">Log In </button>
                     </div>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 my-4 divide-x">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 my-4 divide-zinc-500 divide-x">
                         <div className=" flex justify-center my-2">
                             <Link className="font-semibold sm:font-medium ">Forgot My Username</Link>
-                            {/* <Link className="tracking-tight border-0 sm:items-left items-center sm:border-r border-gray-900 md:pr-8 pr-2 py-2">Forgot My Username</Link> */}
                         </div>
                         <div className="flex justify-center my-2">
-                            {/* <Link className="md:pl-8 pl-2 tracking-tight">Forgot My Password</Link> */}
                             <Link className="font-semibold sm:font-medium">Forgot My Password</Link> 
                         </div>
                     </div>
                     <div className="my-4 text-center">
-                        <Link className="text-base font-extralight">Create An Account</Link>
+                        <Link to="/signup" className="text-base font-extralight">Create An Account</Link>
                     </div>
                     
                 </form>
