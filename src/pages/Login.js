eb118dbe144bfe8ffa4c195840829168432dd3d5
= <mmohmo@yahoo.com>
2024-04-19 21:47:44 -0400

    login page initiated

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div>
+        <section>
+            <div className="w-2/6 bg-white border border-zinc-300">
+                <form>
+
+                </form>
+            </div>
+        </section>
+    </div>
+  )
+}
