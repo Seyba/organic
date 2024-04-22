9864c15dd6299c364783d71872d9f3d7b2bc8f95
= <mmohmo@yahoo.com>
2024-04-21 22:51:39 -0400

    product section background updated

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -18,15 +18,16 @@ export const Products = () => {
                 </div>
             </div>    
         </section>
-        <section>
+        <section className="bg-zinc-200">
             <div className="w-5/6 mx-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-                    <div className="bg-zinc-100 flex justify-center">
+                    <div className="bg-white  flex justify-center">
                         <div>
-                            <h3>product name</h3>
-                            <h4>Product price</h4>
-                            <p>Product description</p>
-                            <button>add to cart</button>
+                            <div className="my-8">
+                                <img src="/images/chicken.png" className="h-22 w-22" alt="" />
+                            </div>
+                            <h4>Whole Chicken</h4>
+                            <button className="bg-green-950 text-white rounded-xl w-3/4">$8</button>
                         </div>
                     </div>
                     <div className="bg-zinc-100 flex justify-center">
