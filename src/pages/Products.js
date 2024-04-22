5ec09c322995adb855f9af473e02b1354c8d483b
= <mmohmo@yahoo.com>
2024-04-21 23:17:07 -0400

    bag and fav button added to product section

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Meta from '../component/Meta'
 import { BreadCrumb } from '../component/BreadCrumb'
+import { Link } from 'react-router-dom'
 export const Products = () => {
   return (
     <>
@@ -21,12 +22,28 @@ export const Products = () => {
         <section className="bg-zinc-200">
             <div className="w-5/6 mx-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-                    <div className="bg-white  flex justify-center">
+                    <div className="bg-white p-4 flex justify-center">
                         <div>
-                            <div className="">
+                            <div className="flex justify-between">
+                                <div className="bg-green-950 rounded-xl px-3 ">
+                                    <Link to="/">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#ffffff" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                                <div className="bg-green-950 rounded-xl px-3 ">
+                                    <Link to="/">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#ffffff" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="w-2/6  mx-auto">
                                 <img src="/images/chicken.png" className="" alt="" />
                             </div>
-                            <h4>Whole Chicken</h4>
+                            <h4 >Whole Chicken</h4>
                             <button className="bg-green-950 text-white rounded-xl w-3/4">$8</button>
                         </div>
                     </div>
