caf878669d1044c927a3b727412871bb2efc7359
= <mmohmo@yahoo.com>
2024-04-22 20:48:06 -0400

    product page banner background updated

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -7,7 +7,7 @@ export const Products = () => {
     <>
         <section className="">
             <div className="relative isolate overflow-hidden py-24 md:py-36 sm:py-32 ">
-                <img src="/images/img3.jpg" alt="" className="absolute inset-0 brightness-50 -z-10 h-full w-full object-cover object-right md:object-center"/>
+                <img src="/images/img4.jpg" alt="" className="absolute inset-0 brightness-50 -z-10 h-full w-full object-cover object-right md:object-center"/>
                 <div className="py-2">
                     <h2 className="text-5xl text-white font-semibold text-center py-2 animate-fade-down animate-once animate-duration-800 animate-ease-in-out">Shop</h2>
                 </div>
@@ -40,11 +40,11 @@ export const Products = () => {
                                     </Link>
                                 </div>
                             </div>
-                            <div className="w-2/6  mx-auto">
+                            <div className="w-2/6 my-16  mx-auto">
                                 <img src="/images/chicken.png" className="" alt="" />
                             </div>
                             <h4 >Whole Chicken</h4>
-                            <button className="bg-green-950 text-white rounded-xl w-3/4">$8</button>
+                            <button className="bg-green-950 text-white mx-auto rounded-xl w-3/4">$8</button>
                         </div>
                     </div>
                     <div className="bg-zinc-100 flex justify-center">
