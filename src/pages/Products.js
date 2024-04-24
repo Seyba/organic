cc631e5da657d5755cee47d136e7e97f2b12383a
= <mmohmo@yahoo.com>
2024-04-23 20:58:53 -0400

    whislist and cart background updated

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -25,14 +25,14 @@ export const Products = () => {
                     <div className="bg-white p-4 flex justify-center">
                         <div>
                             <div className="flex justify-between">
-                                <div className="bg-zinc-300 rounded-full  px-4 ">
+                                <div className="bg-zinc-100 rounded-full  px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-7 h-7">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                         </svg>
                                     </Link>
                                 </div>
-                                <div className="bg-zinc-300 rounded-full px-4 ">
+                                <div className="bg-zinc-100 rounded-full px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
@@ -52,14 +52,14 @@ export const Products = () => {
                     <div className="bg-white p-4 flex justify-center">
                         <div>
                             <div className="flex justify-between">
-                                <div className="bg-zinc-300 rounded-full  px-4 ">
+                                <div className="bg-zinc-100 rounded-full  px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-7 h-7">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                         </svg>
                                     </Link>
                                 </div>
-                                <div className="bg-zinc-300 rounded-full px-4 ">
+                                <div className="bg-zinc-100 rounded-full px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
@@ -79,14 +79,14 @@ export const Products = () => {
                     <div className="bg-white p-4 flex justify-center">
                         <div>
                             <div className="flex justify-between">
-                                <div className="bg-zinc-300 rounded-full  px-4 ">
+                                <div className="bg-zinc-100 rounded-full  px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-7 h-7">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                         </svg>
                                     </Link>
                                 </div>
-                                <div className="bg-zinc-300 rounded-full px-4 ">
+                                <div className="bg-zinc-100 rounded-full px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
@@ -98,7 +98,7 @@ export const Products = () => {
                                 <img src="/images/lambChops.png" className="" alt="" />
                             </div>
                             <h4 className="text-2xl font-semibold text-center mb-4">Lamb Chops</h4>
-                            <div className="bg-zinc-300 mt-4 text-xl font-bold mx-auto py-1 rounded-full text-center w-1/4">
+                            <div className="bg-zinc-100 mt-4 text-xl font-bold mx-auto py-1 rounded-full text-center w-1/4">
                                 <h3 >$20</h3>
                             </div>
                         </div>
@@ -106,14 +106,14 @@ export const Products = () => {
                     <div className="bg-white p-4 flex justify-center">
                         <div>
                             <div className="flex justify-between">
-                                <div className="bg-zinc-300 rounded-full  px-4 ">
+                                <div className="bg-zinc-100 rounded-full  px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-7 h-7">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                         </svg>
                                     </Link>
                                 </div>
-                                <div className="bg-zinc-300 rounded-full px-4 ">
+                                <div className="bg-zinc-100 rounded-full px-4 ">
                                     <Link to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
