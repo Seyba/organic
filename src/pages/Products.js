78636da8d3ac5ceb7f6b315205e5ec292271585b
= <mmohmo@yahoo.com>
2024-04-29 22:22:50 -0400

    product details link added to product image

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -48,7 +48,6 @@ export const Products = () => {
                                 <Link to="/products/product/idosdis">
                                     <img src="/images/chicken.png" className="" alt="" />
                                 </Link>
-                                
                             </div>
                             <h4 className="text-2xl font-semibold text-center mb-4">Whole Chicken</h4>
                             <div className="mt-4 text-lg text-zinc-600 font-bold text-center">
@@ -75,7 +74,10 @@ export const Products = () => {
                                 </div>
                             </div>
                             <div className="w-2/6 my-16  mx-auto">
-                                <img src="/images/freshOkra.png" className="" alt="" />
+                                <Link to="/products/product/idosdis">
+                                    <img src="/images/freshOkra.png" className="" alt="" />
+                                </Link>
+                                
                             </div>
                             <h4 className="text-2xl font-semibold text-center mb-4">Fresh Okra</h4>
                             <div className="mt-4 text-lg text-zinc-600 font-bold text-center">
@@ -102,7 +104,9 @@ export const Products = () => {
                                 </div>
                             </div>
                             <div className="w-2/6 my-16  mx-auto">
-                                <img src="/images/lambChops.png" className="" alt="" />
+                                <Link to="/products/product/idosdis">
+                                    <img src="/images/lambChops.png" className="" alt="" />
+                                </Link>
                             </div>
                             <h4 className="text-2xl font-semibold text-center mb-4">Lamb Chops</h4>
                             <div className="mt-4 text-lg text-zinc-600 font-bold  text-center">
@@ -129,11 +133,74 @@ export const Products = () => {
                                 </div>
                             </div>
                             <div className="w-2/6 my-16  mx-auto">
-                                <img src="/images/bitterTomato.png" className="" alt="" />
+                                <Link to="/products/product/idosdis">
+                                    <img src="/images/bitterTomato.png" className="" alt="" />
+                                </Link>
                             </div>
                             <h4 className="text-2xl font-semibold text-center mb-4">African Eggplant</h4>
                             <div className="mt-4 text-lg text-zinc-600 font-bold mx-auto py-1 text-center">
                                 <h3 >$ 5.00 USD</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-white p-4 flex justify-center">
+                        <div>
+                            <div className="flex justify-between">
+                                <div className="">
+                                    <Link to="/">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                                <div className="">
+                                    <Link to="/">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="w-2/6 my-16  mx-auto">
+                                <Link to="/products/product/idosdis">
+                                    <img src="/images/bitterTomato.png" className="" alt="" />
+                                </Link>
+                            </div>
+                            <h4 className="text-2xl font-semibold text-center mb-4">African Eggplant</h4>
+                            <div className="mt-4 text-lg text-zinc-600 font-bold mx-auto py-1 text-center">
+                                <h3 >$ 5.00 USD</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-white p-4 flex justify-center">
+                        <div>
+                            <div className="flex justify-between">
+                                <div className="">
+                                    <Link to="/">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                                <div className="">
+                                    <Link to="/">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="w-2/6 my-16  mx-auto">
+                                <Link to="/products/product/idosdis">
+                                    <img src="/images/freshOkra.png" className="" alt="" />
+                                </Link>
+                                
+                            </div>
+                            <h4 className="text-2xl font-semibold text-center mb-4">Fresh Okra</h4>
+                            <div className="mt-4 text-lg text-zinc-600 font-bold text-center">
+                                <h3 >$ 8.00 USD</h3>
                             </div>
                         </div>
                     </div>
