fc7bbb027075a0f4064e3b4475fa4f48bd7d75c3
= <mmohmo@yahoo.com>
2024-05-03 22:57:46 -0400

    cart width updated responsively

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -157,7 +157,8 @@ export const Navbar = () => {
                                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                             >
-                                <Dialog.Panel className="relative transform overflow-hidden rounded-sm bg-white text-left shadow-xl transition-all sm:my-8 md:w-full sm:max-w-lg">
+                                <Dialog.Panel className="relative transform overflow-hidden rounded-sm bg-white shadow-xl transition-all md:w-3/6 w-full">
+                                    
                                     <div className="flex justify-between py-4 items-center border border-b-1">
                                         <div className="px-4">
                                             <h3 className="text-2xl font-semibold text-green-950">Your Cart</h3>
@@ -173,23 +174,25 @@ export const Navbar = () => {
                                         </div>
                                     </div>
                                     
+                                    
                                     <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                       <div className="flex justify-between items-center">
-                                        <div>
-                                            <img src="/images/freshCorn.jpg" className="h-16" alt="corn"/>
-                                        </div>
-                                        <div>
-                                            <h4 className="text-xl font-semibold text-green-900">Fresh Corn </h4>
-                                            <h6 className="text-lg font-semibold">$ 10.00 USD</h6>
-                                        </div>
-                                        <div>
-                                            <h4 className="border border-green-900 border-1 text-center bg-gray-50 p-2">2</h4>
-                                            <div>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#ff0000" className="w-4 h-4">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
+                                        
+                                       <div className="block md:flex md:justify-between">
+                                            <div className="block items-left md:flex ">
+                                                <img src="/images/freshCorn.jpg" className="h-16" alt="corn"/>
+                                                <div>
+                                                    <h4 className="text-xl font-semibold text-green-900">Fresh Corn </h4>
+                                                    <h6 className="text-lg font-semibold">$ 10.00 USD</h6>
+                                                </div>
                                             </div>
-                                        </div>
+                                            <div className="block md:flex md:items-center">
+                                                <h4 className="border border-green-900 border-1  bg-gray-50 p-2">2</h4>
+                                                <div className="">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#950000" className="w-4 h-4">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                    </svg>
+                                                </div>
+                                            </div>
                                         </div> 
                                         <div className="sm:flex sm:items-start">
                                             
