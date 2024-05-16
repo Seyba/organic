da0458e50dee49f0a210ee12df2f4c4c2d20adc7
= <mmohmo@yahoo.com>
2024-05-15 20:27:31 -0400

    carousel title updated

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -292,14 +292,14 @@ export const Navbar = () => {
                             {/* Cart */}
                             <div className="flow-root hidden lg:block">
                                 <button className="navbar-cart-btn border border-1 rounded-3xl py-1 px-4">
-                                    <Link onClick={() => setCartOpen(true)}to="/cart"  className="group -m-2 flex justify-between items-center p-2">
+                                    <Link onClick={() => setCartOpen(true)} className="group -m-2 flex justify-between items-center p-2">
                                         <div className="bg-green-950 rounded-full p-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className="w-6 h-6">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                                             </svg>
                                         </div>
                                         <div className="ml-2">
-                                            <h2 className="navbar-cart leading-9 font-bold">Cart 0</h2>
+                                            <h2 className="navbar-cart leading-9 font-bold">Cart 4</h2>
                                         </div>
                                     </Link>
                                 </button>
--- src/component/SoumppouCarousel.js
+++ src/component/SoumppouCarousel.js
@@ -17,11 +17,11 @@ export const SoumppouCarousel = () => {
                             <p className="text-xl tracking-tight font-semibold">100% organic foods in the market</p>
                         </div>
                         <div className="py-6">
-                            <h3 className="text-2xl md:text-6xl font-bold">Halal Meat Source and Vegetable</h3>
-                            <p className="md:text-xl text-lg leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
+                            <h3 className="text-2xl md:text-6xl font-bold">Halal and quality chicken meat</h3>
+                            <p className="md:text-xl text-lg leading-9 font-semibold">Organic eggs source from great quality poultry</p>
                         </div>
                         <div className="flex justify-center mt-3">
-                            <Link to="/shop">
+                            <Link to="/products">
                                 <button className="border border-1 rounded-3xl py-2 md:py-3 px-4 md:px-10 mr-1 hover:bg-slate-100 hover:text-slate-900">Shop Now</button>
                             </Link>
                             <Link to="/contact">
@@ -50,7 +50,7 @@ export const SoumppouCarousel = () => {
                             <p className="md:text-xl text-lg leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
                         </div>
                         <div className="flex justify-center mt-3">
-                            <Link to="/shop">
+                            <Link to="/products">
                                 <button className="border border-1 rounded-3xl py-2 md:py-3 px-4 md:px-10 mr-1 hover:bg-slate-100 hover:text-slate-900">Shop Now</button>
                             </Link>
                             <Link to="/contact">
@@ -79,7 +79,7 @@ export const SoumppouCarousel = () => {
                             <p className="md:text-xl text-lg leading-9 font-semibold">Fresh, Organic and Seasonal Food for a healthy lifestyle</p>
                         </div>
                         <div className="flex justify-center mt-3">
-                            <Link to="/shop">
+                            <Link to="/products">
                                 <button className="border border-1 rounded-3xl py-2 md:py-3 px-4 md:px-10 mr-1 hover:bg-slate-100 hover:text-slate-900">
                                     Shop Now
                                 </button>
